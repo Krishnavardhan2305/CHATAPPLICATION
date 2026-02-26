6edd37bc5e07f69219470f3daa2adff0b876ab5b
Krishnavardhan2305 <captainkv.2511@gmail.com>
2026-02-27 00:41:24 +0530

    changed samesite parameter

--- backend/controllers/usercontroller.js
+++ backend/controllers/usercontroller.js
@@ -75,7 +75,7 @@ export const login = async (req, res) => {
         res.cookie("token", token, { 
             maxAge: 1 * 24 * 60 * 60 * 1000, 
             httpOnly: true, 
-            sameSite: "strict" 
+            sameSite: "None" 
         });
 
         // console.log("Response Headers:", res.getHeaders());
